f25f7f1279629b8d6f288961b8f51503ba877491
Anazodo-C <chukwumanazodo@gmail.com>
2026-06-13 23:04:54 +0100

    push to heroku

--- app/main.py
+++ app/main.py
@@ -25,7 +25,7 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "I am the GOAT, always will be"}
+    return {"message": "I am the GOAT, always"}
 
 # @app.get("/sqlalchemy")
 # def test_posts(db: Session = Depends(get_db)):
